dbc5c9915930bae4fde06db1745609b55ce3a600
cvr-noob <srichaitanyavictims2023@gmail.com>
2024-08-14 18:05:49 +0530

    Chaos reigns!

---
+++ Basics of Java Programming/iDesign/2/5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an integer");
+        int n = scan.nextInt();
+        System.out.println("You entered "+n);
+    }
+}
